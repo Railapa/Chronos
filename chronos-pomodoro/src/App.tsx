cf668abe7b735477be4afec64a28dce9097c013e
Railapa <rai100304@gmail.com>
2026-07-22 12:56:19 -0300

    Chronos

--- chronos-pomodoro/src/App.tsx
+++ chronos-pomodoro/src/App.tsx
@@ -2,8 +2,25 @@ import './styles/global.css'
 import './styles/theme.css'
 
 import { Home } from './pages/Home/index'
+import type { TaskStateModel } from './models/TaslStateModel'
+import { useState } from 'react'
+
+const initialState: TaskStateModel = {
+    tasks: [],
+    secondsRemaining: 0,
+    formattedSecondsRemaining: '00:000',
+    activeTask: null,
+    currentCycle: 0,
+    config: {
+        workTime: 25,
+        shortBreakTime: 5,
+        longBreakTime: 15,
+    }
+}
 
 export const App = () => {
 
-    return <Home/>
+    const [state, setState] = useState(initialState)
+
+    return <Home state={state} setState={setState}/>
 } 
--- chronos-pomodoro/src/pages/Home/index.tsx
+++ chronos-pomodoro/src/pages/Home/index.tsx
@@ -2,8 +2,16 @@ import { MainTemplate } from "../../templates/MainTemplate"
 import { Container } from "../../components/Container/Container"
 import { CountDown } from "../../components/CountDown/CountDown"
 import { MainForm } from "../../components/MainForm/MainForm"
+import type { TaskStateModel } from "../../models/TaslStateModel"
 
-export const Home = () => {
+type HomeProps = {
+    state: TaskStateModel,
+    setState: React.Dispatch<React.SetStateAction<TaskStateModel>>
+}
+
+export const Home = (props: HomeProps) => {
+
+    const { state, setState } = props
 
     return (
         <>
