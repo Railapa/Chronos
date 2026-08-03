bacbf5229e4861802046b166f428a92d79f5a48c
Railapa <rai100304@gmail.com>
2026-08-03 08:53:21 -0300

    Chronos

--- chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
+++ chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
@@ -3,6 +3,7 @@ import { TaskContext } from "./TaskContext";
 import { initialTaskState } from "./initialTaskState";
 import { taskReducer } from "./taskReducer";
 import { TimerWorkerManager } from "../../workers/TimerWorkerManage";
+import { TaskActionTypes } from "./taskActions";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -18,14 +19,22 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     const countDownSeconds = e.data
     console.log(countDownSeconds)
 
-    if(countDownSeconds){
-      console.log('Worker completo')
+    if (countDownSeconds <= 0) {
+      dispatch({
+        type: TaskActionTypes.COMPLETE_TASK,
+      })
       worker.terminate()
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds }
+      })
     }
   })
 
   useEffect(() => {
-    if(!state.activeTask){
+    console.log(state)
+    if (!state.activeTask) {
       console.log('Worker terminado')
       worker.terminate()
     }
--- chronos-pomodoro/src/workers/TimerWorkerManage.ts
+++ chronos-pomodoro/src/workers/TimerWorkerManage.ts
@@ -1,3 +1,6 @@
+import type { TaskStateModel } from "../models/TaslStateModel";
+
+
 let instance: TimerWorkerManager | null = null;
 
 export class TimerWorkerManager {
@@ -15,7 +18,7 @@ export class TimerWorkerManager {
     return instance;
   }
 
-  postMessage(message: any) {
+  postMessage(message: TaskStateModel) {
     this.worker.postMessage(message);
   }
 
